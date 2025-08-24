06cba592ae2058dbf2908ff5e44c24f3bcd7600d
marvin255 <postbox433@gmail.com>
2025-08-24 13:23:26 +0200

    Refactor ValueObject interface to extend Stringable and remove __toString method

--- src/ValueObject.php
+++ src/ValueObject.php
@@ -4,13 +4,8 @@ declare(strict_types=1);
 
 namespace Marvin255\ValueObject;
 
-interface ValueObject
+interface ValueObject extends \Stringable
 {
-    /**
-     * Get string representation of the value object.
-     */
-    public function __toString(): string;
-
     /**
      * Check if the current value object is equal to another value object.
      */
