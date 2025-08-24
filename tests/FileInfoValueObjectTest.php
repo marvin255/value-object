8957ce8f1ec0286c2aec6b2e2170d03f88ccfa6c
marvin255 <postbox433@gmail.com>
2025-08-24 14:31:02 +0200

    File info (#7)
    * Add FileInfoValueObject class and corresponding tests
    
    * Update infection.json to ignore specific methods in FileInfoValueObject and enhance test cases for equality checks

---
+++ tests/FileInfoValueObjectTest.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marvin255\ValueObject\Tests;
+
+use Marvin255\ValueObject\FileInfoValueObject;
+use Marvin255\ValueObject\ValueObject;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+/**
+ * @internal
+ */
+final class FileInfoValueObjectTest extends BaseCase
+{
+    public function testOpenFile(): void
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->expectExceptionMessage('This value object is read-only and does not support modification methods');
+
+        $object = new FileInfoValueObject(__FILE__);
+        $object->openFile();
+    }
+
+    public function testSetFileClass(): void
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->expectExceptionMessage('This value object is read-only and does not support modification methods');
+
+        $object = new FileInfoValueObject(__FILE__);
+        $object->setFileClass();
+    }
+
+    public function testSetInfoClass(): void
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->expectExceptionMessage('This value object is read-only and does not support modification methods');
+
+        $object = new FileInfoValueObject(__FILE__);
+        $object->setInfoClass();
+    }
+
+    public function testToString(): void
+    {
+        $path = '/some/path/to/file.txt';
+        $object = new FileInfoValueObject($path);
+
+        $this->assertSame($path, (string) $object);
+    }
+
+    #[DataProvider('equalsDataProvider')]
+    public function testEquals(ValueObject $object1, ValueObject $object2, bool $expected): void
+    {
+        $this->assertSame($expected, $object1->equals($object2));
+    }
+
+    public static function equalsDataProvider(): array
+    {
+        return [
+            'real same path' => [
+                'object1' => new FileInfoValueObject(__FILE__),
+                'object2' => new FileInfoValueObject(__FILE__),
+                'expected' => true,
+            ],
+            'real different paths' => [
+                'object1' => new FileInfoValueObject(__FILE__),
+                'object2' => new FileInfoValueObject(__DIR__),
+                'expected' => false,
+            ],
+            'real path and not real path' => [
+                'object1' => new FileInfoValueObject(__FILE__),
+                'object2' => new FileInfoValueObject('not-existing-file.txt'),
+                'expected' => false,
+            ],
+            'not real paths, same path' => [
+                'object1' => new FileInfoValueObject('not-existing-file.txt'),
+                'object2' => new FileInfoValueObject('not-existing-file.txt'),
+                'expected' => true,
+            ],
+            'not real paths, different paths' => [
+                'object1' => new FileInfoValueObject('not-existing-file-1.txt'),
+                'object2' => new FileInfoValueObject('not-existing-file-2.txt'),
+                'expected' => false,
+            ],
+            'different types' => [
+                'object1' => new FileInfoValueObject(__FILE__),
+                'object2' => new class() implements ValueObject {
+                    #[\Override]
+                    public function __toString(): string
+                    {
+                        return __FILE__;
+                    }
+
+                    #[\Override]
+                    public function equals(ValueObject $other): bool
+                    {
+                        return true;
+                    }
+                },
+                'expected' => false,
+            ],
+        ];
+    }
+}
